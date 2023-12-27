8d1deac726b6622bdf7fa3af566cc2b3f71090cc
Belupe <108235745+Belupe@users.noreply.github.com>
2023-12-27 10:29:36 +0100

    Update acceso.js

--- acceso.js
+++ acceso.js
@@ -2,13 +2,13 @@
 
 module.exports = {
 
-  tokenGoogle: 'AIzaSyBHIiKeqpQA3vo3CdcCLExT3W9HxY7zVk8',
+  tokenGoogle: 'TOKEN',
 
-  tokenNotion: 'secret_JlonBgPDNQnFYBwYbtoKaOer82IPU6wcM5yY1mSvAS7',
+  tokenNotion: 'TOKEN',
 
-  baseDeDatosNotionId: '970372de988c4095b37293837a205b36',
+  baseDeDatosNotionId: 'ID',
 
-  canalNotificacionesId: "1151555539256484011",
+  canalNotificacionesId: "CANAL",
 
-  BugsID: '1153397045701394523'
+  BugsID: 'BUGSID'
 }
